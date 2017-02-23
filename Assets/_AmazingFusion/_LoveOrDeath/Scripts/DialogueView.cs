6eb7f47a3e286d300e9ac360011f2c728edf9a82
Miguel <mlopez@dreaming-arts.com>
2017-02-23 16:49:44 +0100

    DialogueView.SetText

--- Assets/_AmazingFusion/_LoveOrDeath/Scripts/DialogueView.cs
+++ Assets/_AmazingFusion/_LoveOrDeath/Scripts/DialogueView.cs
@@ -1,29 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 namespace com.AmazingFusion.LoveOrDeath {
     public class DialogueView : Singleton<DialogueView> {
 
-        MoveToEasingAnimation _openAnimation;
+        [SerializeField]
+        EasingAnimation _showAnimation;
+
+        [SerializeField]
+        EasingAnimation _closeAnimation;
+
+        [SerializeField]
+        TMP_Text _text;
 
         public System.Action OnClose;
 
-        protected override void Awake() {
-            base.Awake();
-            _openAnimation = GetComponent<MoveToEasingAnimation>();
+        public void SetText(CharacterAction.ActionResult result) {
+            switch (result) {
+                case CharacterAction.ActionResult.None:
+                    break;
+                case CharacterAction.ActionResult.Win:
+                    break;
+                case CharacterAction.ActionResult.Lose:
+                    break;
+                case CharacterAction.ActionResult.Both:
+                    break;
+            }
         }
 
         public void Show() {
-
+            _showAnimation.Play();
         }
 
         public void Close() {
-            _openAnimation.OnEnd += Closed;
+            _closeAnimation.OnEnd += Closed;
+            _closeAnimation.Play();
         }
 
         void Closed(IEffectable effect) {
-            _openAnimation.OnEnd -= Closed;
+            _closeAnimation.OnEnd -= Closed;
             if (OnClose != null) OnClose();
         }
     }
